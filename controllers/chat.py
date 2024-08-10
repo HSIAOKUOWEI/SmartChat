1e623d1e2e43905ecb56e3293c4edcea8d595eee
XIAOGUOWEI <q985483841@gmail.com>
2024-08-10 13:58:47 +0800

    Update

--- controllers/chat.py
+++ controllers/chat.py
@@ -1,11 +1,9 @@
 from flask import Blueprint, render_template, request, Response
-from models.llm_config.model_list import get_model
-import json
-from models.agent_chat import agent_executor
-from models.crud_history import save_message,get_user_id
-from models.llm_config.agentChat_prompt import initialize_prompt
-import asyncio
 
+from models.llm_config.model_list import get_model
+from models.agent_chat import agentChat_response_sync
+from models.crud_history import save_message,get_user_id
+import json
 
 chat_bp = Blueprint('chat', __name__)
 class StreamWithHeaders(Response):
@@ -47,71 +45,18 @@ def agent_chat():
         print("Received files:", files)
         print("Received images:", images)
         
-
         user_message_content = {"role": "user","content": message}
 
+        # 保存用戶訊息，如對話框為空，則返回新創的對話框id
         dialogue_id = save_message(user_id, dialogue_id, user_message_content)
         headers = {"dialogue_id": dialogue_id} # 返回給前端
-
-        prompt = initialize_prompt(chat_history = history,   # 聊天記錄
-                                   user_message = message,   # 當前消息
-                                   uploaded_files = files,   # 上傳文件
-                                   uploaded_images = images, # 上傳的圖片
-                                   prompt_language="ZH" #EN  # prompt版本
-                                   ) 
-        print(prompt)
-        
-
-        async def generate_response(input, userid, dialogueid):
-            response = "" #用來保存輸出結果 
-            async for event in agent_executor.astream_events({"input": input}, version="v1"):
-                # 加"\n"是因為yield不能保證每一次都是返回一條數據，
-                # 所以用"\n"來區別每一次的數據
-                if event["event"] == "on_chat_model_stream":
-                    content = event["data"]["chunk"].content
-                    response += content
-                    if content:
-                        #使用 jsonify會有問題，因為在返回的時候會把還未完成的異步任務給銷毀掉
-                        yield json.dumps({
-                                            "event": "on_chat_model_stream",
-                                            "content": content
-                                        })+ "\n"
-                elif event["event"] == "on_tool_start":
-                    print(event)
-                    yield json.dumps({
-                                        "event": "on_tool_start",
-                                        "name": event["name"],
-                                        "inputs": event["data"].get("input")
-                                    })+ "\n"
-                    
-                elif event["event"] == "on_tool_end":
-                    yield json.dumps({
-                                        "event": "on_tool_end",
-                                        "name": event["name"],
-                                        "output": event["data"].get("output")
-                                        })+ "\n"
-
-
-            # 保存結果
-            # # print(response)        
-            bot_message_content = {"role": "assistant","content": response}
-            save_message(user_id=userid, dialogue_id=dialogueid, message_content=bot_message_content)
-            
-
-        def generate_response_sync(input, userid, dialogueid):
-            async_gen = generate_response(input=input, userid=userid, dialogueid=dialogueid)
-            loop = asyncio.new_event_loop()
-            asyncio.set_event_loop(loop)
-            try:
-                while True:
-                    yield loop.run_until_complete(async_gen.__anext__())
-            except StopAsyncIteration:
-                pass
-            finally:
-                loop.close()
                 
-        return StreamWithHeaders(generate_response_sync(input=prompt, userid=user_id, dialogueid=dialogue_id), headers=headers, mimetype='text/event-stream')
-
+        return StreamWithHeaders(agentChat_response_sync(userid = user_id, 
+                                                         dialogueid = dialogue_id , 
+                                                         history = history , # 歷史消息
+                                                         message = message,  # 當前消息
+                                                         files = files, 
+                                                         images = images), headers=headers, mimetype='text/event-stream')
         
         # return jsonify({
         #     'success': True,
--- models/agent_chat.py
+++ models/agent_chat.py
@@ -10,7 +10,11 @@ from .llm_config.model_list import get_model
 from .tools_factory.all_tools import tools
 from langchain.agents import AgentExecutor, create_tool_calling_agent
 from langchain_core.prompts import ChatPromptTemplate
+import asyncio
+import json
+from .crud_history import save_message
 
+# prompt定義
 prompt = ChatPromptTemplate.from_messages(
     [
         ("system", "You are a helpful assistant"),
@@ -20,61 +24,106 @@ prompt = ChatPromptTemplate.from_messages(
     ]
 )
 
-
-# llm = get_model(model_type="Google", model_name="gemini-1.5-pro", api_key=google_api_key)
-# llm = get_model(model_type="Groq", model_name="llama3-groq-70b-8192-tool-use-preview", api_key=groq_api_key)
-# llm = get_model(model_type="Groq", model_name="llama-3.1-70b-versatile", api_key=groq_api_key)
-
+# llm初始化
 llm = get_model(model_type="Openai", model_name="gpt-4o-mini", api_key=openai_api_key)
 
+# agent初始化
 agent = create_tool_calling_agent(llm=llm, tools=tools, prompt=prompt)
+
+# agent輸出處理
 agent_executor = AgentExecutor(agent=agent, tools=tools) # ,verbose=True
-# print(agent_executor.invoke({"input": "宜蘭和金門的天氣如何"}))
-# input = "幫我查一下台北天氣"
-# for chunk in agent_executor.stream({"input": input}):
-#     if "actions" in chunk:
-#         for action in chunk["actions"]:
-#             print(f"Calling Tool: `{action.tool}` with input `{action.tool_input}`")
-#     # Observation
-#     elif "steps" in chunk:
-#         for step in chunk["steps"]:
-#             print(f"Tool Result: `{step.observation}`") #api的結果
-#     # Final result
-#     elif "output" in chunk:
-#         print(f'Final Output: {chunk["output"]}')
-#     else:
-#         raise ValueError()
-#     print("---")
 
-# on_chat_model_stream 是模型最終的輸出
-# async def generate_response(input):
-#     async for event in agent_executor.astream_events({"input": input}, version="v1"):
-#         if event["event"] == "on_chain_end":
-#             print(event)
-            # if event["event"] == "on_chat_model_stream":
-            #      continue
-            #     # print(event["data"]["chunck"].content) # chunk一定會有的, 因為有on_chat_model_stream就會有chunk
-                 
-            # #     content = event["data"]["chunk"].content
-            # #     if content:
-            # #         # yield content
-            # #         print(content)
-            # elif event["event"] == "on_tool_start":
-            #     # 開始執行工具
-            #     # print(event)
-            #     # event['name'] 調用工具的名稱 'name': 'taiwai_weather'
-            #     # event["data"]["input"] 工具的輸入 'data': {'input': {'city': '臺南市'}}
-            #     # event["data"].get("input") 用get取值，有些工具不需要輸入參數，get就返回None
-            #     print(event['name'],": ",event["data"].get("input"))
-                
-            # elif event["event"] == "on_tool_end":
-            #     # 工具輸入結束後的回傳結果
-            #     # print(event)
-            #     # event['name'] 調用工具的名稱 'name': 'taiwai_weather'
-            #     # event['ouput'] 工具的輸出
-            #     print( event['name'],": ", event['data'].get('output'))
-            # else:
-            #      print(event)
+# 用戶輸入初始化
+def agent_prompt(chat_history, user_message, uploaded_files, uploaded_images, prompt_language="ZH"):
+    if prompt_language == "ZH":
+        prompt = f"""
+請考慮以下信息以高效且有效地回應用戶的查詢。請考慮聊天記錄、用戶當前的話語以及任何上傳的文件或圖片。如果用戶的查詢與任何上傳的文件或圖片的內容相關，請分析並提供必要的信息，調用相應的工具。但是，如果用戶的查詢不需要上傳的文件或圖片，請優先直接回答他們的問題或使用其他相關工具。
 
-# import asyncio
-# asyncio.run(generate_response(input=input))
+### 聊天記錄
+{chat_history}
+
+### 用戶當前說的話
+{user_message}
+
+### 用戶上傳的檔案
+{uploaded_files}
+
+### 用戶上傳的圖片
+{uploaded_images}
+
+始終優先考慮用戶的當前查詢，只有在直接與用戶問題相關時才利用聊天記錄、文件或圖片。確保回應清晰、準確並有效滿足用戶需求。
+                """
+    else:  # Default to English
+        prompt = f"""
+please consider the following information to respond to the user's query efficiently and effectively. Take into account the chat history, the user's current message, and any uploaded files or images. If the user's query relates to the content of any uploaded files or images, analyze and provide the necessary information by invoking the appropriate tools. However, if the user's query does not require the uploaded files or images, prioritize answering their question directly or use other relevant tools.
+
+### Chat History
+{chat_history}
+
+### User's Current Message
+{user_message}
+
+### Uploaded Files
+{uploaded_files}
+
+### Uploaded Images
+{uploaded_images}
+
+Always prioritize the user's current query and only utilize the chat history, files, or images if directly relevant to the user's question. Ensure the response is clear, accurate, and meets the user's needs effectively.
+                """
+    return prompt
+
+
+async def agentChat_response(input, userid, dialogueid):
+    response = "" #用來保存輸出結果 
+    async for event in agent_executor.astream_events({"input": input}, version="v1"):
+        # 加"\n"是因為yield不能保證每一次都是返回一條數據，
+        # 所以用"\n"來區別每一次的數據
+        if event["event"] == "on_chat_model_stream":
+            content = event["data"]["chunk"].content
+            response += content
+            if content:
+                #使用 jsonify會有問題，因為在返回的時候會把還未完成的異步任務給銷毀掉
+                yield json.dumps({
+                                    "event": "on_chat_model_stream",
+                                    "content": content
+                                })+ "\n"
+        elif event["event"] == "on_tool_start":
+            print(event)
+            yield json.dumps({
+                                "event": "on_tool_start",
+                                "name": event["name"],
+                                "inputs": event["data"].get("input")
+                            })+ "\n"
+            
+        elif event["event"] == "on_tool_end":
+            yield json.dumps({
+                                "event": "on_tool_end",
+                                "name": event["name"],
+                                "output": event["data"].get("output")
+                                })+ "\n"
+
+
+    # 保存結果    
+    bot_message_content = {"role": "assistant","content": response}
+    save_message(user_id=userid, dialogue_id=dialogueid, message_content=bot_message_content)
+    
+
+def agentChat_response_sync(userid, dialogueid, history, message, files, images):
+    prompt = agent_prompt(chat_history = history,   # 聊天記錄
+                                   user_message = message,   # 當前消息
+                                   uploaded_files = files,   # 上傳文件
+                                   uploaded_images = images, # 上傳的圖片
+                                   prompt_language="ZH" #EN  # prompt版本
+                                   ) 
+    
+    async_gen = agentChat_response(input=prompt, userid=userid, dialogueid=dialogueid)
+    loop = asyncio.new_event_loop()
+    asyncio.set_event_loop(loop)
+    try:
+        while True:
+            yield loop.run_until_complete(async_gen.__anext__())
+    except StopAsyncIteration:
+        pass
+    finally:
+        loop.close()
